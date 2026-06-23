cd7ca4438c13faba23a5f1a7c13205f849350fd1
Ashwani24010 <ashwani.singh@modak.com>
2026-06-23 15:06:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success("""Your Smoothie ordered is """+name_of_order+""" !""", icon="✅")
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
     
